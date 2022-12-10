fed64dd34deea9052433fa4cfca1e52ca2c721b7
bka-2cycle <114967375+bka-2cycle@users.noreply.github.com>
2022-12-09 16:02:17 -0800

    finalized html, javascript to a working state and completed the ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # work-day-scheduler
 A calendar that allows you to save events for each hour of the day.
+
+## Description
+
+This is an online daily work day scheduler that allows the user to set appointments and save them to the schedule. The project was created so a user can keep track of appointments by the hour each day. It provides a tool fpr organizing your work day. Building the program allowed me to apply various Javascript features, like utilizing local storage and DayJS.
+
+## Installation
+
+N/A
+
+## Usage
+
+On page load, fill in events, appointements or to-do's for each hour of the day. After you enter data into an hour, click the save icon (disc) on the right in the blue tab. By saving the entered data will stay present until deleted by the user, even if the page is left and returned to or refreshed. The colors for each hour change based on the current hour of the day. All hours shown in grey have passed, the hour shown in red is the current hour and the hours shown in green are in the furure. See screen capture for visual.
+
+![Image of Work Day Planner](assets/Screen-Shot-01.png)
+
+## Credits
+
+Special thanks to Bryan Swarthout, who gave me the instruction to complete the project.
+
+## License
+
+(MIT) Please refer to the license in the Repo.
--- assets/script.js
+++ assets/script.js
@@ -10,31 +10,31 @@ var saveButtonThree = $(".btn-three");
 var saveButtonFour = $(".btn-four");
 var saveButtonFive = $(".btn-five");
 var rows = $(".row");
- 
+
 var descriptionInput = $("#description").value;
 var fieldOfInput = $("textarea");
 
 var descriptions = [];
 
 
-var currentHour = dayjs().format('hh a');
+var currentHour = parseInt(dayjs().format('HH'));
 
 var row = $(".row");
-var pastArr =[];
+var pastArr = [];
 var presentArr = [];
 var futureArr = [];
 
-console.log(currentHour);
+console.log(typeof currentHour);
 
 //determine the color of the rows by current time
-rows.each(function() {
-  var rowHour = $(this).attr("data-hour");
-
+rows.each(function () {
+  var rowHour = parseInt($(this).attr("data-hour"));
+  console.log(rowHour);
   if (rowHour < currentHour) {
     $(this).addClass("row past")
-  }else if(rowHour === currentHour){
+  } else if (rowHour === currentHour) {
     $(this).addClass("row present")
-  }else{
+  } else {
     $(this).addClass("row future")
   }
 });
@@ -42,7 +42,7 @@ rows.each(function() {
 
 // display time at top of calendar    
 function displayTime() {
-  var rightNow = dayjs().format('MMM DD, YYYY hh:mm:ss a');
+  var rightNow = dayjs().format('MMM DD, YYYY hh:mm a');
   dateEl.text(rightNow);
 }
 
@@ -50,151 +50,162 @@ displayTime();
 setInterval(displayTime, 1000);
 
 
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
 // Hour 9
-  $(saveButtonNine).on("click", function(){
-    var user = $("#description-9").val();
-    console.log("The save button was clicked.");
-    console.log(user);
-    getInputFromTextBoxNine(user);
-  });
-  
-  function getInputFromTextBoxNine(inputValue) {
-    localStorage.setItem("description-9", JSON.stringify(inputValue));
-  } 
+$(saveButtonNine).on("click", function () {
+  var user = $("#description-9").val();
+  console.log("The save button was clicked.");
+  console.log(user);
+  getInputFromTextBoxNine(user);
+});
 
-  // Hour 10
-  $(saveButtonTen).on("click", function(){
-    var user = $("#description-10").val();
-    console.log("The save button was clicked.");
-    console.log(user);
-    getInputFromTextBoxTen(user);
-  });
-
-  function getInputFromTextBoxTen(inputValue) {
-    localStorage.setItem("description-10", JSON.stringify(inputValue));
-  } 
-
-  // Hour 11
-  $(saveButtonElleven).on("click", function(){
-    var user = $("#description-11").val();
-    console.log("The save button was clicked.");
-    console.log(user);
-    getInputFromTextBoxElleven(user);
-  });
-
-  function getInputFromTextBoxElleven(inputValue) {
-    localStorage.setItem("description-11", JSON.stringify(inputValue));
-  } 
-
-  // Hour 12
-  $(saveButtonTwelve).on("click", function(){
-    var user = $("#description-12").val();
-    console.log("The save button was clicked.");
-    console.log(user);
-    getInputFromTextBoxTwelve(user);
-  });
-
-  function getInputFromTextBoxTwelve(inputValue) {
-    localStorage.setItem("description-12", JSON.stringify(inputValue));
-  } 
-
-  // Hour 1
-  $(saveButtonOne).on("click", function(){
-    var user = $("#description-1").val();
-    console.log("The save button was clicked.");
-    console.log(user);
-    getInputFromTextBoxOne(user);
-  });
-
-  function getInputFromTextBoxOne(inputValue) {
-    localStorage.setItem("description-1", JSON.stringify(inputValue));
-  } 
-
-  // Hour 2
-  $(saveButtonTwo).on("click", function(){
-    var user = $("#description-2").val();
-    console.log("The save button was clicked.");
-    console.log(user);
-    getInputFromTextBoxTwo(user);
-  });
-
-  function getInputFromTextBoxTwo(inputValue) {
-    localStorage.setItem("description-2", JSON.stringify(inputValue));
-  } 
-
-  // Hour 3
-  $(saveButtonThree).on("click", function(){
-    var user = $("#description-3").val();
-    console.log("The save button was clicked.");
-    console.log(user);
-    getInputFromTextBoxThree(user);
-  });
-
-  function getInputFromTextBoxThree(inputValue) {
-    localStorage.setItem("description-3", JSON.stringify(inputValue));
-  } 
-
-  // Hour 4
-  $(saveButtonFour).on("click", function(){
-    var user = $("#description-4").val();
-    console.log("The save button was clicked.");
-    console.log(user);
-    getInputFromTextBoxFour(user);
-  });
-
-  function getInputFromTextBoxFour(inputValue) {
-    localStorage.setItem("description-4", JSON.stringify(inputValue));
-  } 
-
-  // Hour 5
-  $(saveButtonFive).on("click", function(){
-    var user = $("#description-5").val();
-    console.log("The save button was clicked.");
-    console.log(user);
-    getInputFromTextBoxFive(user);
-  });
-
-  function getInputFromTextBoxFive(inputValue) {
-    localStorage.setItem("description-5", JSON.stringify(inputValue));
-  } 
-
-
-//console.log(descriptionInput);
+function getInputFromTextBoxNine(inputValue) {
+  localStorage.setItem("9", inputValue);
+}
+//keep data on refresh-9
+var hourNine = localStorage.getItem("9");
+console.log(hourNine);
+document.getElementById("description-9").value = hourNine;
 
 
 
+// Hour 10
+$(saveButtonTen).on("click", function () {
+  var user = $("#description-10").val();
+  console.log("The save button was clicked.");
+  console.log(user);
+  getInputFromTextBoxTen(user);
+});
 
-  /* function getInputFromTextBox() {
-    localStorage.setItem("description", descriptionInput);
-    console.log(descriptionInput);
-    //localStorage.getItem(descriptionInput);
-  }; */
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+function getInputFromTextBoxTen(inputValue) {
+  localStorage.setItem("10", inputValue);
+}
+//keep data on refresh-10
+var hourTen = localStorage.getItem("10");
+console.log(hourTen);
+document.getElementById("description-10").value = hourTen;
 
 
+
+// Hour 11
+$(saveButtonElleven).on("click", function () {
+  var user = $("#description-11").val();
+  console.log("The save button was clicked.");
+  console.log(user);
+  getInputFromTextBoxElleven(user);
+});
+
+function getInputFromTextBoxElleven(inputValue) {
+  localStorage.setItem("11", inputValue);
+}
+//keep data on refresh-11
+var hourElleven = localStorage.getItem("11");
+console.log(hourElleven);
+document.getElementById("description-11").value = hourElleven;
+
+
+
+// Hour 12
+$(saveButtonTwelve).on("click", function () {
+  var user = $("#description-12").val();
+  console.log("The save button was clicked.");
+  console.log(user);
+  getInputFromTextBoxTwelve(user);
+});
+
+function getInputFromTextBoxTwelve(inputValue) {
+  localStorage.setItem("12", inputValue);
+}
+//keep data on refresh-12
+var hourTwelve = localStorage.getItem("12");
+console.log(hourTwelve);
+document.getElementById("description-12").value = hourTwelve;
+
+
+
+// Hour 1
+$(saveButtonOne).on("click", function () {
+  var user = $("#description-1").val();
+  console.log("The save button was clicked.");
+  console.log(user);
+  getInputFromTextBoxOne(user);
+});
+
+function getInputFromTextBoxOne(inputValue) {
+  localStorage.setItem("1", inputValue);
+}
+//keep data on refresh-1
+var hourOne = localStorage.getItem("1");
+console.log(hourOne);
+document.getElementById("description-1").value = hourOne;
+
+
+
+// Hour 2
+$(saveButtonTwo).on("click", function () {
+  var user = $("#description-2").val();
+  console.log("The save button was clicked.");
+  console.log(user);
+  getInputFromTextBoxTwo(user);
+});
+
+function getInputFromTextBoxTwo(inputValue) {
+  localStorage.setItem("2", inputValue);
+}
+//keep data on refresh-2
+var hourTwo = localStorage.getItem("2");
+console.log(hourTwo);
+document.getElementById("description-2").value = hourTwo;
+
+
+
+// Hour 3
+$(saveButtonThree).on("click", function () {
+  var user = $("#description-3").val();
+  console.log("The save button was clicked.");
+  console.log(user);
+  getInputFromTextBoxThree(user);
+});
+
+function getInputFromTextBoxThree(inputValue) {
+  localStorage.setItem("3", inputValue);
+}
+//keep data on refresh-3
+var hourThree = localStorage.getItem("3");
+console.log(hourThree);
+document.getElementById("description-3").value = hourThree;
+
+
+
+// Hour 4
+$(saveButtonFour).on("click", function () {
+  var user = $("#description-4").val();
+  console.log("The save button was clicked.");
+  console.log(user);
+  getInputFromTextBoxFour(user);
+});
+
+function getInputFromTextBoxFour(inputValue) {
+  localStorage.setItem("4", inputValue);
+}
+//keep data on refresh-4
+var hourFour = localStorage.getItem("4");
+console.log(hourFour);
+document.getElementById("description-4").value = hourFour;
+
+
+
+// Hour 5
+$(saveButtonFive).on("click", function () {
+  var user = $("#description-5").val();
+  console.log("The save button was clicked.");
+  console.log(user);
+  getInputFromTextBoxFive(user);
+});
+
+function getInputFromTextBoxFive(inputValue) {
+  localStorage.setItem("5", inputValue);
+}
+//keep data on refresh-5
+var hourFive = localStorage.getItem("5");
+console.log(hourFive);
+document.getElementById("description-5").value = hourFive;
